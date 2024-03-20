120863468e278e02947335b5e9cf707db5faedfd
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-03-20 07:09:26 +0300

    last update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log('hello')
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+let operator = document.getElementById('operator')
+let result = document.getElementById('result');
+
+function operation(){
+    let finalR
+
+    switch (operator.value) {
+        case '+':
+             finalR = parseInt(num1.value) + parseInt(num2.value);
+            break;
+        case '-':
+             finalR = parseInt(num1.value) - parseInt(num2.value);
+            break;
+        case '*':
+             finalR = parseInt(num1.value) * parseInt(num2.value);
+            break;
+        case '/':
+             finalR = parseInt(num1.value) / parseInt(num2.value);
+            break;
+        default:
+            break;
+    }
+   
+    result.innerHTML = finalR
+}
